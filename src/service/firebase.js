28d5f57e4a6cc13a429353ee310b473d58f3ee09
seanwuvely <seanwuvely@gmail.com>
2021-04-02 09:49:57 +0900

    project setup and add firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
