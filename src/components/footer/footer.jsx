eaaace9b31ceb794a37488e48ac1ddb54b9bc673
seanwuvely <seanwuvely@gmail.com>
2021-04-02 10:59:55 +0900

    add editor and preview components

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import styles from './footer.module.css';
+import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <p className={styles.title}>Code your dream</p>
+    <p className={styles.title}>@seanwuvely</p>
   </footer>
 );
 
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import Footer from "../footer/footer";
 import Header from "../header/header";
@@ -7,6 +7,41 @@ import Preview from "../preview/preview";
 import styles from "./maker.module.css";
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "a",
+      company: "a",
+      theme: "dark",
+      title: "Software Engineer",
+      email: "a@gmail.com",
+      message: "a",
+      fileName: "a",
+      fileURL: null,
+    },
+    {
+      id: "2",
+      name: "b",
+      company: "b",
+      theme: "light",
+      title: "Software Engineer",
+      email: "b@gmail.com",
+      message: "b",
+      fileName: "b",
+      fileURL: "b.png",
+    },
+    {
+      id: "3",
+      name: "c",
+      company: "c",
+      theme: "colorful",
+      title: "c",
+      email: "c@gmail.com",
+      message: "c",
+      fileName: "c",
+      fileURL: null,
+    },
+  ]);
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -19,12 +54,16 @@ const Maker = ({ authService }) => {
       }
     });
   });
+
+  const addCard = (card) => {
+    console.log(card);
+  };
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} addCard={addCard} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
