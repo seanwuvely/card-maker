d0cb411db28faba2ed7dc17948a4ae38549bf7de
seanwuvely <seanwuvely@gmail.com>
2021-04-02 11:25:46 +0900

    handle state change on editor

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,68 +1,87 @@
-import React, { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
-import Footer from "../footer/footer";
-import Header from "../header/header";
-import Editor from "../editor/editor";
-import Preview from "../preview/preview";
-import styles from "./maker.module.css";
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+import Editor from '../editor/editor';
+import Preview from '../preview/preview';
+import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
-  const [cards, setCards] = useState([
-    {
-      id: "1",
-      name: "a",
-      company: "a",
-      theme: "dark",
-      title: "Software Engineer",
-      email: "a@gmail.com",
-      message: "a",
-      fileName: "a",
+  const [cards, setCards] = useState({
+    1: {
+      id: '1',
+      name: 'Ellie',
+      company: 'Samsung',
+      theme: 'dark',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
       fileURL: null,
     },
-    {
-      id: "2",
-      name: "b",
-      company: "b",
-      theme: "light",
-      title: "Software Engineer",
-      email: "b@gmail.com",
-      message: "b",
-      fileName: "b",
-      fileURL: "b.png",
+    2: {
+      id: '2',
+      name: 'Ellie2',
+      company: 'Samsung',
+      theme: 'light',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: 'ellie.png',
     },
-    {
-      id: "3",
-      name: "c",
-      company: "c",
-      theme: "colorful",
-      title: "c",
-      email: "c@gmail.com",
-      message: "c",
-      fileName: "c",
+    3: {
+      id: '3',
+      name: 'Ellie3',
+      company: 'Samsung',
+      theme: 'colorful',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
       fileURL: null,
     },
-  ]);
+  });
+
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
   };
 
   useEffect(() => {
-    authService.onAuthChange((user) => {
+    authService.onAuthChange(user => {
       if (!user) {
-        history.push("/");
+        history.push('/');
       }
     });
   });
 
-  const addCard = (card) => {
-    console.log(card);
+  const createOrUpdateCard = card => {
+    setCards(cards => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+      return updated;
+    });
   };
+
+  const deleteCard = card => {
+    setCards(cards => {
+      const updated = { ...cards };
+      delete updated[card.id];
+      return updated;
+    });
+  };
+
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
+        <Editor
+          cards={cards}
+          addCard={createOrUpdateCard}
+          updateCard={createOrUpdateCard}
+          deleteCard={deleteCard}
+        />
         <Preview cards={cards} />
       </div>
       <Footer />
